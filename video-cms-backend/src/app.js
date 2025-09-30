ca7ee2123f172a6c23d25c0f2e8fa3b4d96aef49
Shamail Abbas <shamailabbas61@gmail.com>
2025-09-30 18:58:22 +0500

    updated

--- video-cms-backend/src/app.js
+++ video-cms-backend/src/app.js
@@ -1,10 +1,16 @@
 import express from "express";
+import cors from "cors";
 import mediaRoutes from "./routes/media.js";
 import { connectDB } from "./db.js";
 import MediaModel from "./models/Media.js";
 import { config } from "./config.js";
 
 const app = express();
+
+// Enable CORS for all origins
+app.use(cors());
+
+// Parse JSON request bodies
 app.use(express.json());
 
 const PORT = config.Port;
@@ -20,7 +26,7 @@ const startServer = async () => {
     // Make Media globally available (for controllers)
     app.set("models", { Media });
 
-    // Routes (now have access to Media)
+    // Routes
     app.use("/media", mediaRoutes);
 
     app.listen(PORT, () => {
