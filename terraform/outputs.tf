3334145a315b949ee2dd30d7cf07b6f0425dca2f
Shamail Abbas <shamailabbas61@gmail.com>
2025-10-07 00:39:17 +0500

    update output.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,16 +1,12 @@
-# EC2
 output "ec2_bastion_public_ip" {
   value       = aws_instance.bastion.public_ip
-  description = "Public IP of the backend EC2 instance"
+  description = "Public IP of the bastion EC2 instance"
 }
-output "ec2_backend_public_ip" {
+output "ec2_backend_private_ip" {
   value       = [for instance in aws_instance.backend : instance.private_ip]
-  description = "Public IP of the backend EC2 instance"
+  description = "Private IP of the backend EC2 instances"
 }
 
-
-
-# S3 & CloudFront
 output "s3_bucket_name" {
   value       = aws_s3_bucket.media_bucket.id
   description = "S3 bucket name"
@@ -21,21 +17,15 @@ output "cloudfront_domain_name" {
   description = "CloudFront distribution domain name"
 }
 
-# RDS
-output "rds_endpoint" {
-  value       = aws_db_instance.postgres.address
-  description = "RDS endpoint"
-}
-
 output "db_secret_arn" {
   value       = aws_secretsmanager_secret.db_secret.arn
   description = "Secrets Manager ARN for DB credentials"
 }
 
-# Redis
-output "redis_endpoint" {
+
+output "redis_host_endpoint" {
   value       = aws_elasticache_cluster.redis.cache_nodes[0].address
-  description = "Redis endpoint"
+  description = "Redis host endpoint"
 }
 
 
